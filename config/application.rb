917b7ff90ed42aa248d3e8989082938084b3cdef
Andrew Mason <ajm188@case.edu>
2014-03-14 13:44:58 -0400

    fix deploy issue

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative File.expand_path('../boot', __FILE__)
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
-require_relative File.expand_path('../application', __FILE__)
+require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SRC::Application.initialize!
