13a52eaec172fa6d4060572500dc3a90af243c7e
Andrew Mason <ajm188@case.edu>
2014-03-14 20:47:15 -0400

    update gemfile lock for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :application, 'SpartanRunningClub'
 set :repo_url, 'git@github.com:ajm188/SpartanRunningClub.git'
 
 set :deploy_to, '/home/andrew/SpartanRunningClub'
-set :deploy_via, :copy
+set :deploy_via, :remote_cache
 
 set :scm, :git
 set :branch, :master
