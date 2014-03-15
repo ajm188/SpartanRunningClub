2f501c62e095d484df439d4b690e1bd995cefc9e
Andrew Mason <ajm188@case.edu>
2014-03-14 23:17:30 -0400

    finish deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,17 +23,21 @@ set :keep_releases, 3
 
 set :pty, true
 
+set :stage, :production
+
+set :linked_files, %w{config/database.yml}
+
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+    desc 'Restart application'
+    task :restart do
+        on roles(:app), in: :sequence, wait: 5 do
+            # Your restart mechanism here, for example:
+            execute :touch, release_path.join('tmp/restart.txt')
+        end
     end
-  end
 
-  after :publishing, :restart
+    after :publishing, :restart
 end
 
 desc "Copy bootstrap tasks to current release on server"
