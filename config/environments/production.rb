a4719f37d059dddbc3e6847a53ab07cd92741296
Andrew Mason <ajm188@case.edu>
2014-03-15 23:48:35 -0400

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,9 @@ SRC::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.stmp_settings = {
+  config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
     :port                 => 587,
     :domain               => 'running.case.edu',
