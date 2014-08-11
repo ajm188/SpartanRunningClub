2b7358195a2f3f1fb661137993433de016b0fb6d
Andrew Mason <ajm188@case.edu>
2014-08-11 08:46:07 -0400

    add date_string and time_string methods for meetings

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,12 @@
 class Meeting < ActiveRecord::Base
   validates :title, :date, :time,
     presence: true, allow_blank: false
+
+  def date_string
+    self.date.present? ? self.date.strftime('%m/%d/%Y') : nil
+  end
+
+  def time_string
+    self.time.present? ? self.time.strftime('%l:%M %p').strip : nil
+  end
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -4,4 +4,56 @@ RSpec.describe Meeting, :type => :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:date) }
   it { should validate_presence_of(:time) }
+
+  describe '#date_string' do
+    before(:all) do
+      @meeting = FactoryGirl.build(:meeting)
+    end
+
+    context 'when date is set' do
+      before(:all) do
+        @meeting.date = Date.strptime('12/25/2014', '%m/%d/%Y')
+      end
+
+      it 'should return string representing the date' do
+        expect(@meeting.date_string).to eq '12/25/2014'
+      end
+    end
+
+    context 'when date is nil' do
+      before(:all) do
+        @meeting.date = nil
+      end
+
+      it 'should return nil' do
+        expect(@meeting.date_string).to be nil
+      end
+    end
+  end
+
+  describe '#time_string' do
+    before(:all) do
+      @meeting = FactoryGirl.build(:meeting)
+    end
+
+    context 'when time is set' do
+      before(:all) do
+        @meeting.time = Time.strptime('5:30 PM', '%l:%M %p')
+      end
+
+      it 'should return string representing the time' do
+        expect(@meeting.time_string).to eq '5:30 PM'
+      end
+    end
+
+    context 'when time is nil' do
+      before(:all) do
+        @meeting.time = nil
+      end
+
+      it 'should return nil' do
+        expect(@meeting.time_string).to be nil
+      end
+    end
+  end
 end
