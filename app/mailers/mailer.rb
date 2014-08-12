6b35effabdf3cf506a04b3df685360e38e9f8a14
Andrew Mason <ajm188@case.edu>
2014-08-12 09:09:26 -0400

    create mailer superclass for sharing globals

---
+++ app/mailers/mailer.rb
@@ -0,0 +1,4 @@
+class Mailer < ActionMailer::Base
+  FROM = 'running-club@case.edu'
+  URL = 'running.case.edu'
+end
--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,15 +1,22 @@
-class MemberMailer < ActionMailer::Base
-	default from: "running-club@case.edu"
+class MemberMailer < Mailer
 
-	def welcome_email member
-		@member = member
-		@url = 'running.case.edu'
-		mail(to: @member.email, from: 'running-club@case.edu', subject: 'Welcome to Spartan Running Club!')
-	end
+  def welcome_email member
+    @member = member
+    @url = 'running.case.edu'
+    mail(
+      to: @member.email,
+      from: FROM,
+      subject: 'Welcome to Spartan Running Club!'
+    )
+  end
 
-	def change_password_email member
-		@member = member
-		@url = 'running.case.edu'
-		mail(:to => @member.email, :from => 'running-club@case.edu', :subject => 'Your password has been changed')
-	end
+  def change_password_email member
+    @member = member
+    @url = URL
+    mail(
+      to: @member.email,
+      from: FROM,
+      subject: 'Your password has been changed'
+    )
+  end
 end
