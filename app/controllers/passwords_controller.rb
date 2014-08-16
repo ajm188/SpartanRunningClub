6f18eb66f62affc099169f58f603fa7fc8376ded
Andrew Mason <ajm188@case.edu>
2014-08-15 23:02:36 -0400

    verify old password actually matches the one in the database

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -11,14 +11,14 @@ class PasswordsController < Clearance::PasswordsController
 	def user_edit
 		@member = Member.find(params[:id].to_i)
 		if current_user.id == @member.id
-			#old_pass = params[:password][:old_pass]
+			old_pass = params[:password][:old_pass]
 			new_pass = params[:password][:new_pass]
 			confirm_pass = params[:password][:confirm_pass]
 			errors = []
 
-			#unless @member.password == old_pass
-				#errors << "Old password does not match your password"
-			#end
+			unless BCrypt::Password.new(@member.encrypted_password) == old_pass
+				errors << "Old password does not match your password"
+			end
 
 			unless new_pass == confirm_pass
 				errors << "New passwords do not match"
