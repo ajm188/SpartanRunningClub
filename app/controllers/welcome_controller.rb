51040c0c4fa05f032ad06c99aad829b71478a9f6
Andrew Mason <ajm188@case.edu>
2014-08-19 19:58:53 -0400

    welcome controller cleanup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,13 @@ class WelcomeController < ApplicationController
   skip_before_filter :authorize, only: [:home, :contact, :about]
 
   def home
-  	@active = CarouselItem.first_index
-  	@items = CarouselItem.rest
+    @active = CarouselItem.first_index
+    @items = CarouselItem.rest
     @upcoming_events = Event.upcoming.limit(5)
   end
 
   def contact
-  	@officers = Member.officers
+    @officers = Member.officers
   end
 
   def about
