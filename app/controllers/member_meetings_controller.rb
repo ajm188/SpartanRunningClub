e6905e7999091f8540a799b34698f2895ee907d7
Andrew Mason <ajm188@case.edu>
2014-08-22 15:49:00 -0400

    better add/remove for meeting invites

--- app/controllers/member_meetings_controller.rb
+++ app/controllers/member_meetings_controller.rb
@@ -1,5 +1,6 @@
 class MemberMeetingsController < ApplicationController
   before_filter :authorize_as_officer
+  before_action :set_member_meeting, only: [:destroy]
   
   def new
     @updates = {
@@ -7,11 +8,33 @@ class MemberMeetingsController < ApplicationController
         'member_meetings/new' }
     }
     @relationship = params[:relationship]
+    @meeting_id = params[:meeting_id]
     render 'shared/update'
   end
 
   def create
-    @member = Member.find(params[:member_id])
-    @relationship = params[:relationship]
+    @member_meeting = MemberMeeting.new(member_meeting_params)
+    @member_meeting.invitor = current_user unless
+      @member_meeting.relationship == MemberMeeting::ATTENDEE
+    @member_meeting.save
+  end
+
+  def destroy
+    @member_meeting.destroy
+  end
+
+  private
+
+  def set_member_meeting
+    @member_meeting = MemberMeeting.where(
+      relationship: params[:relationship],
+      meeting_id: params[:meeting_id],
+      member_id: params[:member_id]
+    ).first
+  end
+
+  def member_meeting_params
+    params.require(:member_meeting)
+          .permit(:relationship, :meeting_id, :member_id)
   end
 end
