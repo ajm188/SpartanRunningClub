53f18383f1c9cb2db2fdef9a4d72e0a4c85e4686
Andrew Mason <ajm188@case.edu>
2014-08-22 20:31:18 -0400

    article specs

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ActiveRecord::Base
+  include Followable
+  
+  belongs_to :author, class_name: 'Member'
+  belongs_to :editor, class_name: 'Member'
+
+  validates :title, :body, :author_id, :editor_id,
+    presence: true, allow_blank: false
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:author_id) }
+  it { should validate_presence_of(:editor_id) }
+
+  it { should belong_to(:author) }
+  it { should belong_to(:editor) }
+
+  describe '::Followable' do
+    it { should have_many(:followings) }
+    it { should have_many(:members) }
+  end
 end
