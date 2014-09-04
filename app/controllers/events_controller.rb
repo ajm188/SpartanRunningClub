97df35fab699863e3751fed3411ac8eb09fae92e
Andrew Mason <ajm188@case.edu>
2014-09-04 12:38:45 -0400

    add photo to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -67,7 +67,7 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:name, :date, :time, :description)
+    params.require(:event).permit(:name, :date, :time, :description, :photo)
   end
 
   def parse_date
