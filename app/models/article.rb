835eab40cbaf22c1c0c4da0e7c38d409e32bf6b4
Andrew Mason <ajm188@case.edu>
2014-10-05 19:37:47 -0400

    included Timed module for articles. aliased the update_at attribute so the Timed module works correctly

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,12 @@
 class Article < ActiveRecord::Base
   include Followable
+  include Timed
   
   belongs_to :author, class_name: 'Member'
   belongs_to :editor, class_name: 'Member'
 
   validates :title, :body, :author_id, :editor_id,
     presence: true, allow_blank: false
+
+  alias_attribute :time, :updated_at
 end
