80417e1f669a727218e999424214a1b2d2561061
Andrew Mason <ajm188@case.edu>
2014-10-05 19:42:53 -0400

    added method to show last edited time with the editor for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,4 +9,11 @@ class Article < ActiveRecord::Base
     presence: true, allow_blank: false
 
   alias_attribute :time, :updated_at
+
+
+  def last_edited_string
+    self.time_string +
+      (self.author_id != self.editor_id ?
+        " (by #{self.editor.full_name})" : "")
+  end
 end
