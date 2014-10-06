21a3c1fe397ba23d9985ffe0dcb37c23366c4b9f
Andrew Mason <ajm188@case.edu>
2014-10-06 00:00:28 -0400

    validations and corresponding specs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :commenter, class_name: 'Member'
   belongs_to :commentable, polymorphic: true
+
+  validates :comment, :commenter_id, :commentable_id, :commentable_type,
+    presence: true, allow_blank: false
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
+  it { should validate_presence_of :comment }
+  it { should validate_presence_of :commenter_id }
+  it { should validate_presence_of :commentable_id }
+  it { should validate_presence_of :commentable_type }
+
   it { should belong_to :commenter }
   it { should belong_to :commentable }
 end
