57659e25d5138d81f4383897ad859bff7f87df81
Andrew Mason <ajm188@case.edu>
2014-10-06 10:41:31 -0400

    included Dated and Timed modules in comments. using created_at for both the date and time

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,13 @@
 class Comment < ActiveRecord::Base
+  include Dated
+  include Timed
+
   belongs_to :commenter, class_name: 'Member'
   belongs_to :commentable, polymorphic: true
 
   validates :comment, :commenter_id, :commentable_id, :commentable_type,
     presence: true, allow_blank: false
+
+  alias_attribute :date, :created_at
+  alias_attribute :time, :created_at
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,4 +8,28 @@ RSpec.describe Comment, :type => :model do
 
   it { should belong_to :commenter }
   it { should belong_to :commentable }
+
+  describe '::Dated' do
+    before(:all) do
+      @comment = Comment.new(created_at: DateTime.strptime('08/26/2015', '%m/%d/%Y'))
+    end
+
+    it { should respond_to :date_string }
+
+    it 'should use created_at for the date attribute' do
+      expect(@comment.date_string).to eq '08/26/2015'
+    end
+  end
+
+  describe '::Timed' do
+    before(:all) do
+      @comment = Comment.new(created_at: DateTime.strptime('12:30 PM', '%I:%M %p'))
+    end
+
+    it { should respond_to :time_string }
+
+    it 'should use created_at for the time attribute' do
+      expect(@comment.time_string).to eq '12:30 PM'
+    end
+  end
 end
